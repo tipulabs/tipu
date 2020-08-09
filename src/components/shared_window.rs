fd9693f7d2df2ca93a11390104618090311aaaa4
aryaminus <sunim.54@gmail.com>
2020-08-09 16:39:03 +0545

    FIX #5 ✨ Add the message feature and comment

--- src/components/shared_window.rs
+++ src/components/shared_window.rs
@@ -1,3 +1,10 @@
+use std::thread;
+use std::fs::File;
+use std::time::Duration;
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::io::{self, ErrorKind, Read, Write};
+
 use tui::{
     backend::Backend,
     layout::Rect,
@@ -23,6 +30,9 @@ impl<'a> Default for SharedWindow<'a> {
     }
 }
 
+const MSG_SIZE: usize = 32;
+const LOCAL: &str = "https://protected-everglades-84717.herokuapp.com:80";
+
 impl<'a> SharedWindow<'a> {
     pub fn is_focused(mut self, is_focused: bool) -> SharedWindow<'a> {
         self.is_focused = is_focused;
@@ -44,6 +54,53 @@ impl<'a> SharedWindow<'a> {
             .title(self.title)
             .border_type(BorderType::Plain);
 
+            // let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+            // client
+            //     .set_nonblocking(true)
+            //     .expect("Failed to initiate non-blocking");
+        
+            // let (tx, rx) = mpsc::channel::<String>();
+        
+            // thread::spawn(move || loop {
+            //     let mut buff = vec![0; MSG_SIZE];
+            //     match client.read_exact(&mut buff) {
+            //         Ok(_) => {
+            //             let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+            //             println!("Received message: {:?}", String::from_utf8(msg).unwrap());
+            //         }
+            //         Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            //         Err(_) => {
+            //             println!("Connection with server lost.");
+            //             break;
+            //         }
+            //     }
+        
+            //     match rx.try_recv() {
+            //         Ok(msg) => {
+            //             let mut buffer = msg.clone().into_bytes();
+            //             buffer.resize(MSG_SIZE, 0);
+            //             client.write_all(&buffer).expect("Writing to socket failed");
+            //             println!("Sent message: {:?}", msg);
+            //         }
+            //         Err(TryRecvError::Empty) => (),
+            //         Err(TryRecvError::Disconnected) => break,
+            //     }
+        
+            //     thread_sleep();
+            // });
+        
+            // println!("Write a Message:");
+            // loop {
+            //     let mut buff = String::new();
+            //     io::stdin()
+            //         .read_line(&mut buff)
+            //         .expect("Reading from stdin failed.");
+            //     let msg = buff.trim().to_string();
+            //     if msg == ":q" || tx.send(msg).is_err() {
+            //         break;
+            //     }
+            // }
+
         if self.is_focused {
             block = Block::default()
                 .borders(Borders::ALL)
@@ -54,4 +111,8 @@ impl<'a> SharedWindow<'a> {
 
         self
     }
+}
+
+fn thread_sleep() {
+    thread::sleep(Duration::from_millis(120));
 }

55c804a5785f598aa50a0dbaa936d022f57be434
aryaminus <sunim.54@gmail.com>
2020-08-09 16:38:45 +0545

    FIX #5 🔨 Add the terminal codebase and comment it

--- src/components/terminal.rs
+++ src/components/terminal.rs
@@ -1,3 +1,6 @@
+use std::io::{self, Write};
+use std::process::{self, Command};
+
 use tui::{
     backend::Backend,
     layout::Rect,
@@ -6,6 +9,8 @@ use tui::{
     widgets::{Block, BorderType, Borders},
     Frame,
 };
+
+use crate::util::{parse};
 
 pub struct Terminal<'a> {
     size: Rect,
@@ -44,6 +49,19 @@ impl<'a> Terminal<'a> {
             .title(self.title)
             .border_type(BorderType::Plain);
 
+        // let mut input = String::new();
+        // print!("$ ");
+        // io::stdout().flush().unwrap();
+        // match io::stdin().read_line(&mut input) {
+        //     Ok(0) => break,
+        //     Ok(_) => if let Some((path, args)) = eval_input(input.trim()) {
+        //         exec_input(&path, args.iter().map(AsRef::as_ref).collect());
+        //     },
+        //     Err(_) => process::exit(1),
+        // }
+        // input.clear();
+        // println!("Bye!");
+
         if self.is_focused {
             block = Block::default()
                 .borders(Borders::ALL)
@@ -53,5 +71,36 @@ impl<'a> Terminal<'a> {
         f.render_widget(block, self.size);
 
         self
+    }
+}
+
+fn eval_input(input: &str) -> Option<(String, Vec<String>)> {
+    match parse::parse(input) {
+        Ok(mut expr) => {
+            let args = expr
+                .split_off(1)
+                .iter()
+                .map(|x| x.unwrap())
+                .collect();
+            let path = expr[0].unwrap();
+
+            Some((path, args))
+        },
+        Err(msg) => {
+            eprintln!("error: {}", msg);
+            None
+        },
+    }
+}
+
+
+fn exec_input(path: &str, args: Vec<&str>) {
+    match Command::new(path).args(args).spawn() {
+        Ok(mut child) => if let Ok(exit_status) = child.wait() {
+            println!("process exited with code {}", exit_status.code().unwrap_or(0));
+        },
+        Err(e) => {
+            println!("{}", e);
+        },
     }
 }
